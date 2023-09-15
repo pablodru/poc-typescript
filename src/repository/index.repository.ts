649db3059bd4d656f434288aafba8cf86ca023a2
pablodru <pdrocha03@gmail.com>
2023-09-15 20:08:38 -0300

    fix: typing CRUD queries

--- src/repository/index.repository.ts
+++ src/repository/index.repository.ts
@@ -1,24 +1,24 @@
 import { db } from "@/database/database.connection";
-import { Team } from "@/protocols/protocols";
+import { Team, idDelete } from "@/protocols/protocols";
 
 async function postTeamRepository (team : Team) {
     const query = `INSERT INTO team ("pointGuard", "shootingGuard", "smallForward", "powerForward", center) VALUES ( $1, $2, $3, $4, $5 )`
-    return await db.query(query, [team.pointGuard, team.shootingGuard, team.smallForward, team.powerForward, team.center]);
+    return await db.query<Team>(query, [team.pointGuard, team.shootingGuard, team.smallForward, team.powerForward, team.center]);
 }
 
 async function getTeamRepository () {
     const query = `SELECT * FROM team LIMIT 10`
-    return await db.query<Team[]>(query);
+    return await db.query<Team>(query);
 }
 
 async function updateTeamRepository (team: Team) {
     const query = `UPDATE team SET "pointGuard" = $1 , "shootingGuard" = $2, "smallForward" = $3, "powerForward" = $4, center = $5`
-    return await db.query(query, [team.pointGuard, team.shootingGuard, team.smallForward, team.powerForward, team.center]);
+    return await db.query<Team>(query, [team.pointGuard, team.shootingGuard, team.smallForward, team.powerForward, team.center]);
 }
 
 async function deleteTeamRepository (id: string) {
     const query = `DELETE FROM team WHERE id = $1`
-    return await db.query(query, [id]);
+    return await db.query<idDelete>(query, [id]);
 }
 
 const repository = { postTeamRepository, getTeamRepository, updateTeamRepository, deleteTeamRepository };
